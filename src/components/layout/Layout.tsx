3f8a5c8511bf57ff54d6d12cecd2b656f6737733
sopoour <sophia.auer@googlemail.com>
2025-11-25 15:18:57 +0100

    Subpages

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -5,19 +5,36 @@ import Header from './Header';
 import Footer from './Footer';
 import Sidebar from '../Sidebar';
 import { flexColumn } from '@app/styles/mixins';
+import { Main } from 'next/document';
 
 const Root = styled.div`
   min-height: 100vh;
-  display: flex;
-  flex-direction: column;
+  ${flexColumn};
+  width: 100%;
+  background: url('leo_pattern.png');
+  background-repeat: repeat;
+  background-size: 500px 500px;
+  background-position: top left;
+  z-index: 1;
+
+  &::before {
+    content: '';
+    position: absolute;
+    height: 100vh;
+    width: 100%;
+    background: linear-gradient(135deg, #6d5cdb, #fa56a8);
+    opacity: 0.8;
+  }
 `;
 
 const MainLayout = styled.main`
-  max-width: 1000px;
-  padding: 48px 20px;
-  flex: 1;
+  position: relative;
+  height: 100vh;
   width: 100%;
-  margin: 0 auto;
+  ${flexColumn};
+  z-index: 2;
+  max-width: 1000px;
+  padding: 48px;
 `;
 
 type Props = {
@@ -26,9 +43,11 @@ type Props = {
 
 const Layout: FC<Props> = ({ children }) => (
   <Root>
-    <Sidebar>Some content</Sidebar>
-    <Header />
-    {children}
+    <MainLayout>
+      <Sidebar>Some content</Sidebar>
+      <Header />
+      {children}
+    </MainLayout>
     <Footer />
   </Root>
 );
